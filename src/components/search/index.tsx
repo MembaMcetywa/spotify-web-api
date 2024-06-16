f2a8cd3ad4f451e7c4f234cf9d8e6174701515ae
Membathisi <membathisi.mcetywa@smokeci.com>
2024-06-16 10:25:07 +0200

    changes before search component overhaul

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -5,10 +5,14 @@ import { Artist } from '../../models';
 interface SearchProps {
   token: string | null;
   setArtists: (artists: Artist[]) => void;
+  playlistId: string;
+  setTracks: (tracks: any[]) => void;
 }
 
 const Search: FC<SearchProps> = ({ token, setArtists }) => {
   const [searchKey, setSearchKey] = useState<string>('');
+  const [allTracks, setAllTracks] = useState<any[]>([]);
+  const PLAYLIST_ID =import.meta.env.VITE_PLAYLIST_ID;
 
   const searchArtists = async () => {
     if (token && searchKey) {
@@ -29,7 +33,38 @@ const Search: FC<SearchProps> = ({ token, setArtists }) => {
     }
   };
 
+  const searchPlaylist = async () => {
+    if (token) {
+      try {
+  
+        const { data: playlist } = await axios.get(`https://api.spotify.com/v1/playlists/${PLAYLIST_ID}`, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+  
+        // Fetch tracks in the playlist
+        const { data: { items: tracks } } = await axios.get(`https://api.spotify.com/v1/playlists/${PLAYLIST_ID}/tracks`, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+  
+        console.log("Playlist:", playlist);
+        console.log("Tracks:", tracks);
+      } catch (error: any) {
+        if (error.response && error.response.status === 401) {
+          console.error("Unauthorized. Please log in again.");
+          window.localStorage.removeItem("token");
+        } else {
+          console.error("Error occurred:", error.message);
+        }
+      }
+    }
+  };
+
   useEffect(() => {
+    searchPlaylist();
     // If there's a need to auto-search or debounce, handle that here
   }, [token, searchKey]); // Dependent on token and searchKey to re-run
 
